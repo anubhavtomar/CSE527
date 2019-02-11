ff0dbf815d4c6922423eb6283db7158b36ef03f5
Anubhav Tomar <anubhav.tomar94@gmail.com>
2019-02-10 21:43:52 -0500

    HW0 commit

--- HW0.py
+++ HW0.py
@@ -16,7 +16,7 @@ image.shape
 image[0,0]
 
 import matplotlib.pyplot as plt
-plt.imshow(image[0:10,0:10])
+plt.imshow(image[0:200,0:200])
 
 plt.imshow(cv2.cvtColor(image.copy(), cv2.COLOR_RGB2GRAY), cmap='gray')
 
@@ -45,17 +45,33 @@ plt.imshow(cv2.GaussianBlur(image.copy(), ksize=(11,11), sigmaX=-1))
 #------------------------------------------------------------------------------
 
 
+# TODO: your code here
 grayImage = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
 grayImage = grayImage.astype(np.float32) / 255.
 
 boxFilter = cv2.boxFilter(grayImage , ddepth = -1 , ksize=(11 , 11))
-gaussFilter = cv2.GaussianBlur(grayImage , ksize=(11,11) , sigmaX=-1)
+gaussFilter = cv2.GaussianBlur(grayImage , ksize=(11 , 11) , sigmaX=-1)
 
 boxRes = grayImage - boxFilter
 gaussRes = grayImage - gaussFilter
 
-plt.subplot(1,2,1),plt.imshow(boxRes, cmap='gray'),plt.colorbar()
-plt.subplot(1,2,2),plt.imshow(gaussRes, cmap='gray'),plt.colorbar()
+f = plt.figure(figsize=(20, 7.5))
+plt.subplot(1,2,1) , plt.title('Using Box Filter 11X11') , plt.imshow(boxRes, cmap='gray') , plt.colorbar()
+plt.subplot(1,2,2) , plt.title('Using Gauss Filter 11X11') , plt.imshow(gaussRes, cmap='gray') , plt.colorbar()
+
+# Different kernel size
+grayImage = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
+grayImage = grayImage.astype(np.float32) / 255.
+
+boxFilter = cv2.boxFilter(grayImage , ddepth = -1 , ksize=(31 , 31))
+gaussFilter = cv2.GaussianBlur(grayImage , ksize=(31 , 31) , sigmaX=-1)
+
+boxRes = grayImage - boxFilter
+gaussRes = grayImage - gaussFilter
+
+f = plt.figure(figsize=(20, 7.5))
+plt.subplot(1,2,1) , plt.title('Using Box Filter 31X31') , plt.imshow(boxRes, cmap='gray') , plt.colorbar()
+plt.subplot(1,2,2) , plt.title('Using Gauss Filter 31X31') , plt.imshow(gaussRes, cmap='gray') , plt.colorbar()
 
 
 #------------------------------------------------------------------------------
@@ -68,23 +84,13 @@ plt.subplot(1,2,2),plt.imshow(gaussRes, cmap='gray'),plt.colorbar()
 #------------------------------------------------------------------------------
 
 
+# TODO: your code here
 hsvImage = cv2.cvtColor(image.copy(), cv2.COLOR_RGB2HSV)
-#hsvImage = image.copy()
-hChannel = hsvImage.copy()
-hChannel[:,:,1] = 0
-hChannel[:,:,2] = 0
-
-sChannel = hsvImage.copy()
-sChannel[:,:,0] = 0
-sChannel[:,:,2] = 0
-
-vChannel = hsvImage.copy()
-vChannel[:,:,0] = 0
-vChannel[:,:,1] = 0
-
-plt.subplot(1,3,1),plt.imshow(hChannel)
-plt.subplot(1,3,2),plt.imshow(sChannel)
-plt.subplot(1,3,3),plt.imshow(vChannel)
+hChannel , sChannel , vChannel = cv2.split(hsvImage);
+f = plt.figure(figsize=(20, 10))
+plt.subplot(1,3,1) , plt.title('H Component') , plt.imshow(hChannel)
+plt.subplot(1,3,2) , plt.title('S Component') , plt.imshow(sChannel)
+plt.subplot(1,3,3) , plt.title('V Component') , plt.imshow(vChannel)
 
 pts3d = np.hstack([np.random.uniform(-5,5,(1000,2)),np.random.uniform(5,100,(1000,1))])[:,np.newaxis,:]
 from mpl_toolkits.mplot3d import Axes3D
@@ -106,23 +112,20 @@ K = np.array([[800,0,320],[0,800,240],[0,0,1]], dtype=np.float32)
 #------------------------------------------------------------------------------
 
 
-pts2d = cv2.projectPoints(pts3d , rvec = (0,0,0) , tvec = (0,0,0) , cameraMatrix = K , distCoeffs = None)
-myPts2d = []
-for pt in pts3d:
-    newPt = pt.transpose()
-    mult = np.matmul(K , newPt)
-    myPts2d.append([mult[0][0]/mult[2][0] , mult[1][0]/mult[2][0]])
-    
-fig = plt.figure(figsize=(10,6))
-ax1 = fig.add_subplot(121 , projection = '3d')
-ax1.set_title("Using Matrix Multiplication")
-ax1.scatter(myPts2d[:] , myPts2d[:] , cmap='gray')
-ax1.set_aspect('equal')
+# TODO: your coded here
+pts2d = cv2.projectPoints(pts3d , rvec = (0,0,0) , tvec = (0,0,0) , cameraMatrix = K , distCoeffs = None)    
+newPts3d = np.resize(pts3d , (1000,3))
+pts2dMatMul = np.matmul(K , np.transpose(newPts3d))
+for i in range(1000):
+    pts2dMatMul[:,i] = pts2dMatMul[:,i]/pts2dMatMul[2,i]
+f = plt.figure(figsize = (20,5))
 
-ax2 = fig.add_subplot(122 , projection = '3d')
-ax2.set_title("Using `cv2.projectPoints`")
-ax2.scatter(pts2d[0][:] , pts2d[0][:] , cmap='gray')
-ax2.set_aspect('equal')
+K = np.array([[500,0,120],[0,500,40],[0,0,1]], dtype=np.float32)
+pts2dDiffK = cv2.projectPoints(pts3d , rvec = (0,0,0) , tvec = (0,0,0) , cameraMatrix = K , distCoeffs = None)    
+
+plt.subplot(131) , plt.title("Using cv2.projectPoints") , plt.scatter(pts2d[0][:,:,0] , pts2d[0][:,:,1] , color='red')
+plt.subplot(132) , plt.title("Using np.matmul") , plt.scatter(pts2dMatMul[0,:] , pts2dMatMul[1,:] , color='green')
+plt.subplot(133) , plt.title("After changeing K") , plt.scatter(pts2dDiffK[0][:,:,0] , pts2dDiffK[0][:,:,1] , color='blue')
 
 # cube veritces
 Z = np.array([[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-1],[-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1]])
@@ -149,8 +152,77 @@ ax.set_aspect('equal') # uniform scale axes
 
 #Use the same method from before to project the 3D points to 2D
 pts2d = None 
-pts2d = []
+# TODO: your code here
 K = np.array([[800,0,320],[0,800,240],[0,0,1]], dtype=np.float32)
-for pt in Z:
-    mult = np.matmul(K , pt)
-    pts2d.append([mult[0]/mult[2] , mult[1]/mult[2]])
+pts2d = cv2.projectPoints(np.float32(Z) , rvec = (0,0,0) , tvec = (0,0,0) , cameraMatrix = K , distCoeffs = None)[0]
+
+faces2d = [[pts2d[0],pts2d[1],pts2d[2],pts2d[3]],
+           [pts2d[4],pts2d[5],pts2d[6],pts2d[7]], 
+           [pts2d[0],pts2d[1],pts2d[5],pts2d[4]], 
+           [pts2d[2],pts2d[3],pts2d[7],pts2d[6]], 
+           [pts2d[1],pts2d[2],pts2d[6],pts2d[5]],
+           [pts2d[4],pts2d[7],pts2d[3],pts2d[0]]]
+
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+patches = []
+for p in np.array(faces2d):
+    patches += [Polygon(np.squeeze(p), True)]
+
+fig=plt.figure()
+ax = fig.add_subplot(111)
+ax.add_collection(PatchCollection(patches, alpha=0.1, linewidths=1, edgecolors='r'))
+ax.scatter(pts2d[:,0,0],pts2d[:,0,1],c=-Z[:,2])
+ax.set_xlim(0,640)
+ax.set_ylim(0,480)
+
+#Try to change the translation of the 3D points, as well as the K matrix, and see how it affects the 2D projection.
+
+# cube veritces
+Z = np.array([[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-1],[-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1]])
+Z[:,0] += 5 # translate on X
+Z[:,1] += 5 # translate on Y
+Z[:,2] += 10 # translate on Z
+
+# list of faces
+faces = [[Z[0],Z[1],Z[2],Z[3]],
+         [Z[4],Z[5],Z[6],Z[7]], 
+         [Z[0],Z[1],Z[5],Z[4]], 
+         [Z[2],Z[3],Z[7],Z[6]], 
+         [Z[1],Z[2],Z[6],Z[5]],
+         [Z[4],Z[7],Z[3],Z[0]]]
+
+# Plot the cube in 3D
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.add_collection3d(Poly3DCollection(faces, facecolors='cyan', linewidths=1, edgecolors='r', alpha=.75))
+ax.scatter(Z[:,0], Z[:,1], Z[:,2])
+ax.set_xlim(0,11), ax.set_ylim(0,11), ax.set_zlim(0,11)
+ax.set_aspect('equal') # uniform scale axes
+
+# New Value of K
+K = np.array([[500,0,0],[0,300,0],[0,0,1]], dtype=np.float32)
+pts2d = cv2.projectPoints(np.float32(Z) , rvec = (0,0,0) , tvec = (0,0,0) , cameraMatrix = K , distCoeffs = None)[0]
+
+# list of 2D faces
+faces2d = [[pts2d[0],pts2d[1],pts2d[2],pts2d[3]],
+           [pts2d[4],pts2d[5],pts2d[6],pts2d[7]], 
+           [pts2d[0],pts2d[1],pts2d[5],pts2d[4]], 
+           [pts2d[2],pts2d[3],pts2d[7],pts2d[6]], 
+           [pts2d[1],pts2d[2],pts2d[6],pts2d[5]],
+           [pts2d[4],pts2d[7],pts2d[3],pts2d[0]]]
+
+# from matplotlib.patches import Polygon
+# from matplotlib.collections import PatchCollection
+patches = []
+for p in np.array(faces2d):
+    patches += [Polygon(np.squeeze(p), True)]
+
+fig=plt.figure()
+ax = fig.add_subplot(111)
+ax.add_collection(PatchCollection(patches, alpha=0.1, linewidths=1, edgecolors='r'))
+ax.scatter(pts2d[:,0,0],pts2d[:,0,1],c=-Z[:,2])
+ax.set_xlim(0,640)
+ax.set_ylim(0,480)
